c0661b4acb1d11ae2f25f26d342191d92fb746ba
Eduardo Wallace da Silva <eduardo.wallace@hdi.com.br>
2020-02-26 17:37:53 -0300

    ajuste

--- src/main/java/com/ews/camelxmldemo/CamelController.java
+++ src/main/java/com/ews/camelxmldemo/CamelController.java
@@ -6,10 +6,15 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.camel.CamelContext;
 import org.apache.camel.ProducerTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+/**
+ *  curl -XGET -s http://localhost:8080/camel/route
+ */
 @Slf4j
 @RestController
 @RequestMapping("/camel")
@@ -20,9 +25,11 @@ public class CamelController {
 
     ObjectMapper mapper = new ObjectMapper();
 
-
     @GetMapping(value = "/route")
     JsonNode route() {
+
+        //CamelContext camelContext = (CamelContext) new ClassPathXmlApplicationContext("classpath:camel/camel-context.xml");
+
         final ProducerTemplate producerTemplate = camelContext.createProducerTemplate();
         JsonNode json = (JsonNode) producerTemplate.requestBody("direct:httpMultcastAsync", "hey");
         log.info("Response called Http Multicast : " + json.toString());
--- src/main/java/com/ews/camelxmldemo/CamelXmlDemoApplication.java
+++ src/main/java/com/ews/camelxmldemo/CamelXmlDemoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
-@ImportResource("classpath:my-camel.xml")
+@ImportResource("classpath:camel-context.xml")
 public class CamelXmlDemoApplication {
 
 	public static void main(String[] args) {
