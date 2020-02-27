11a2aa779e34f3a92422c2bf22f64d50f70867ba
Eduardo Wallace da Silva <eduardo.wallace@hdi.com.br>
2020-02-27 11:37:04 -0300

    adjustment

--- src/main/java/com/ews/camelxmldemo/CamelController.java
+++ src/main/java/com/ews/camelxmldemo/CamelController.java
@@ -1,12 +1,10 @@
 package com.ews.camelxmldemo;
 
 import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.camel.CamelContext;
 import org.apache.camel.ProducerTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,13 +21,8 @@ public class CamelController {
     @Autowired
     private CamelContext camelContext;
 
-    ObjectMapper mapper = new ObjectMapper();
-
     @GetMapping(value = "/route")
     JsonNode route() {
-
-        //CamelContext camelContext = (CamelContext) new ClassPathXmlApplicationContext("classpath:camel/camel-context.xml");
-
         final ProducerTemplate producerTemplate = camelContext.createProducerTemplate();
         JsonNode json = (JsonNode) producerTemplate.requestBody("direct:httpMultcastAsync", "hey");
         log.info("Response called Http Multicast : " + json.toString());
